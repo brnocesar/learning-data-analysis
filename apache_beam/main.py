024ca31f87de7bd8affb62b6a3a229413f7f6fd4
Bruno Cesar <bruunoocesar@gmail.com>
2022-08-10 12:36:24 -0300

    Passa cada linha do arquivo para lista, cada coluna é um item da lista

--- apache_beam/main.py
+++ apache_beam/main.py
@@ -5,6 +5,10 @@ from apache_beam.io import ReadFromText
 file_path_dengue = '../datasets/apache_beam_casos_dengue.txt'
 
 #%%
+# passa uma linha (string) para lista, onde cada coluna se torna um item
+row_to_list  = lambda row, sep='|': row.split(sep)
+
+#%%
 opts     = PipelineOptions(argv=None)
 pipeline = beam.Pipeline(options=opts)
 
@@ -12,6 +16,7 @@ pipeline = beam.Pipeline(options=opts)
 dengue = (
     pipeline
     | "Leitura do dataset de dengue" >> ReadFromText(file_path_dengue, skip_header_lines=1) # passo 1: leitura do arquivo, recupera cada linha como uma string
+    | "De texto para lista" >> beam.Map(row_to_list) # passo 2: separa as colunas na string em uma lista
     | "Mostrar resultados" >> beam.Map(print)
 )
 
