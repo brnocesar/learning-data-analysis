7b3a45ed11365e2c1684e45847fc3e74f758159a
Bruno Cesar <bruunoocesar@gmail.com>
2022-08-10 12:39:17 -0300

    Passa cada linha do arquivo para um dicionário, onde as chaves são os nomes das colunas no arquivo

--- apache_beam/main.py
+++ apache_beam/main.py
@@ -9,6 +9,14 @@ file_path_dengue = '../datasets/apache_beam_casos_dengue.txt'
 row_to_list  = lambda row, sep='|': row.split(sep)
 
 #%%
+with open(file_path_dengue, "r") as file:
+    first_line = file.readline().replace('\n', '')
+    file.close()
+
+# lista com os nomes das colunas no arquivo
+labels_dengue = row_to_list(first_line)
+
+#%%
 opts     = PipelineOptions(argv=None)
 pipeline = beam.Pipeline(options=opts)
 
@@ -17,6 +25,7 @@ dengue = (
     pipeline
     | "Leitura do dataset de dengue" >> ReadFromText(file_path_dengue, skip_header_lines=1) # passo 1: leitura do arquivo, recupera cada linha como uma string
     | "De texto para lista" >> beam.Map(row_to_list) # passo 2: separa as colunas na string em uma lista
+    | "De lista para dicionário" >> beam.Map(list_to_dict, labels_dengue) # passo 3: monta um dicionário com os elementos da lista
     | "Mostrar resultados" >> beam.Map(print)
 )
 
